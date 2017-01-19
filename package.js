27b856bbc3047b70289c45a0ea5e2fecd2d67116
Abigail Watson <awatson1978@gmail.com>
2017-01-19 16:09:22 -0600

    RiskAssessments

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-risk-assessment',
-  version: '1.3.4',
+  version: '1.3.5',
   summary: 'HL7 FHIR Resource - Risk Assessment',
   git: 'https://github.com/clinical-meteor/hl7-resource-risk-assessment',
   documentation: 'README.md'

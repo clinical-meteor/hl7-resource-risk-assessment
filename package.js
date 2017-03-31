a1d565a1abd6fe868c2d054aba09578dc2a0152c
Abigail Watson <awatson1978@gmail.com>
2017-03-31 18:46:46 -0500

    DTSU3 datatypes; fhirVersion

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-risk-assessment',
-  version: '1.3.5',
+  version: '1.3.8',
   summary: 'HL7 FHIR Resource - Risk Assessment',
   git: 'https://github.com/clinical-meteor/hl7-resource-risk-assessment',
   documentation: 'README.md'
@@ -14,10 +14,10 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('prime8consulting:meteor-oauth2-server@0.0.2');
+  api.use('clinical:fhir-vault-server@0.0.3');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.6.0');
+  api.use('clinical:hl7-resource-datatypes@3.0.0');
 
   api.addFiles('lib/hl7-resource-risk-assessment.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
--- server/rest.js
+++ server/rest.js
@@ -1,13 +1,17 @@
+var fhirVersion = 'fhir-3.0.0';
 
-JsonRoutes.Middleware.use(
-    '/api/*',
+if(typeof oAuth2Server === 'object'){
+  // TODO:  double check that this is needed; and that the /api/ route is correct
+  JsonRoutes.Middleware.use(
+    // '/api/*',
+    '/fhir-3.0.0/*',
     oAuth2Server.oauthserver.authorise()   // OAUTH FLOW - A7.1
-);
+  );
+}
 
 
 
-
-JsonRoutes.add("get", "/fhir/RiskAssessment/:id", function (req, res, next) {
+JsonRoutes.add("get", "/" + fhirVersion + "/RiskAssessment/:id", function (req, res, next) {
   process.env.DEBUG && console.log('GET /fhir/RiskAssessment/' + req.params.id);
 
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -43,7 +47,7 @@ JsonRoutes.add("get", "/fhir/RiskAssessment/:id", function (req, res, next) {
 
 
 
-JsonRoutes.add("get", "/fhir/RiskAssessment", function (req, res, next) {
+JsonRoutes.add("get", "/" + fhirVersion + "/RiskAssessment", function (req, res, next) {
   process.env.DEBUG && console.log('GET /fhir/RiskAssessment', req.query);
 
   res.setHeader("Access-Control-Allow-Origin", "*");

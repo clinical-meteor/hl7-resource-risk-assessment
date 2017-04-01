e16cf94dc503a4c7b516d6af59de6fc50a55c5c5
Abigail Watson <awatson1978@gmail.com>
2017-03-31 22:19:57 -0500

    mocha integration test

---
+++ tests/integration.tests.js
@@ -0,0 +1,15 @@
+import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+import { chai } from 'meteor/practicalmeteor:chai';
+
+describe('clinical:hl7-resource-risk-assessment', function () {
+  beforeEach(function () {
+    console.log('beforeEach');
+  });
+  afterEach(function () {
+    console.log('afterEach');
+  });
+  it('renders correctly with simple data', function () {
+    chai.assert.equal(true, true);
+  });
+});

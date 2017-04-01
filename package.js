4fbbd0277422dee29771df73818a9bffee3fb1d8
Abigail Watson <awatson1978@gmail.com>
2017-03-31 19:13:00 -0500

    circle.yml updates

--- package.js
+++ package.js
@@ -14,7 +14,6 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('clinical:fhir-vault-server@0.0.3');
 
   api.use('clinical:base-model@1.3.5');
   api.use('clinical:hl7-resource-datatypes@3.0.0');
@@ -22,6 +21,12 @@ Package.onUse(function (api) {
   api.addFiles('lib/hl7-resource-risk-assessment.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
+
+  // clinical:fhir-vault-server is a licensed product
+  // so may not exist
+  try {
+    api.use('clinical:fhir-vault-server@0.0.3');    
+  } 
 
   api.export('RiskAssessment');
   api.export('RiskAssessments');

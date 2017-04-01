235bdb72b7d15bb59a48dd1878c429393ada07ae
Abigail Watson <awatson1978@gmail.com>
2017-03-31 23:19:52 -0500

    updated integration tests

--- tests/integration.tests.js
+++ tests/integration.tests.js
@@ -1,15 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 import { assert } from 'meteor/practicalmeteor:chai';
 import { chai } from 'meteor/practicalmeteor:chai';
+import { expect } from 'meteor/practicalmeteor:chai';
 
 describe('clinical:hl7-resource-risk-assessment', function () {
   beforeEach(function () {
-    console.log('beforeEach');
+    //console.log('beforeEach');
   });
   afterEach(function () {
-    console.log('afterEach');
+    //console.log('afterEach');
   });
   it('renders correctly with simple data', function () {
-    chai.assert.equal(true, true);
+    expect(RiskAssessments).to.exist;
   });
 });

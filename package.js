58ca0e96fa30d4d5213ad2f6d9305b4ff76a50df
Abigail Watson <awatson1978@gmail.com>
2017-04-12 16:40:00 -0500

    updated datatypes to include Meta.fhirVersion

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-risk-assessment',
-  version: '1.4.1',
+  version: '1.4.2',
   summary: 'HL7 FHIR Resource - Risk Assessment',
   git: 'https://github.com/clinical-meteor/hl7-resource-risk-assessment',
   documentation: 'README.md'
@@ -16,14 +16,14 @@ Package.onUse(function (api) {
   api.use('simple:json-routes@2.1.0');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('clinical:hl7-resource-datatypes@3.0.1');
 
   api.addFiles('lib/hl7-resource-risk-assessment.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
 
   if(Package['clinical:fhir-vault-server']){
-    api.use('clinical:fhir-vault-server@0.0.3');        
+    api.use('clinical:fhir-vault-server@0.0.3');
   }
 
   api.export('RiskAssessment');

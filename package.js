9390e91cda37c1f25d25a6833a138146c996b008
Abigail Watson <awatson1978@gmail.com>
2019-10-13 16:22:00 -0500

    updated circle config

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-risk-assessment',
-  version: '1.5.4',
+  version: '1.5.5',
   summary: 'HL7 FHIR Resource - Risk Assessment',
   git: 'https://github.com/clinical-meteor/hl7-resource-risk-assessment',
   documentation: 'README.md'
@@ -23,7 +23,7 @@ Package.onUse(function (api) {
   api.use('http');
   api.use('react-meteor-data@0.2.15');
 
-  api.use('clinical:glass-ui@2.1.4');
+  api.use('clinical:glass-ui@2.4.7');
   api.use('clinical:extended-api@2.2.2');
   api.use('clinical:base-model@1.4.0');
   api.use('clinical:user-model@1.6.2');
